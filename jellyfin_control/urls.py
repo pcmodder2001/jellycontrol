851b15bbae7c1eae5c70a20ac99135a1e49a67df
brad <brad.crampton2001@gmail.com>
2025-01-23 19:49:07 +0000

    Main

--- jellyfin_control/urls.py
+++ jellyfin_control/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('test-email/', views.test_email, name='test_email'),
+    path('download-database/', views.download_database, name='download_database'),
+    path('upload-database/', views.upload_database, name='upload_database'),
 ] 
